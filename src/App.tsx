474ec2824d9aa9ec0d1ad6b52f92aa20b0f1a433
codecues <tharifmail@gmail.com>
2025-06-12 16:21:53 +0530

    fixed

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>V0 Earned Values</h1>
+      <p>This is a working Vite + React starter.</p>
+    </div>
+  );
+}
